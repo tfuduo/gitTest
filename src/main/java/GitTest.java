c8348c0c6fb1d475019fd40a50e30145a1d8df1d
oatuh <329794799@qq.com>
2020-01-17 19:38:46 +0800

    修改为加法

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,6 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         int a = 5;
         int b = 2;
-        System.out.println(a - b);
+        System.out.println(a + b);
     }
 }
